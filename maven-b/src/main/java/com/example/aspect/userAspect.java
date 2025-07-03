403fef26a337acff15f9c8382cf178141cb57e03
kevin <kevin3691@163.com>
2025-07-03 22:12:19 +0800

    测试1

--- maven-b/src/main/java/com/example/aspect/userAspect.java
+++ maven-b/src/main/java/com/example/aspect/userAspect.java
@@ -19,12 +19,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class userAspect{
 
-    @Pointcut("@annotation(com.example.annotation.userCheck)")
-    public void userPointcut() {
-        // 定义切点，匹配所有带有 @userCheck 注解的方法
-    }
+//    @Pointcut("@annotation(com.example.annotation.userCheck)")
+//    public void userPointcut() {
+//        // 定义切点，匹配所有带有 @userCheck 注解的方法
+//    }
 
-    @Around("userPointcut()")
+    @Around("@annotation(com.example.annotation.userCheck)")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("Around 前置操作：" + joinPoint.getSignature().getName());
         Object result = joinPoint.proceed(); // 执行原方法
@@ -32,10 +32,10 @@ public class userAspect{
         return result;
     }
 
-    @Before("userPointcut()")
-    public void before(JoinPoint joinPoint) {
-        // 在方法执行前进行用户校验
-        System.out.println("用户校验切面：方法 " + joinPoint.getSignature().getName() + " 被调用");
-        // 这里可以添加具体的用户校验逻辑
-    }
+//    @Before("userPointcut()")
+//    public void before(JoinPoint joinPoint) {
+//        // 在方法执行前进行用户校验
+//        System.out.println("用户校验切面：方法 " + joinPoint.getSignature().getName() + " 被调用");
+//        // 这里可以添加具体的用户校验逻辑
+//    }
 }
